64d8675e215b9184bbe724d8ad73e76cbf48057c
DamzelDeesse <damzeld@gmail.com>
2021-06-04 22:17:28 -0400

    Make it make sense

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MainContent from './topcontent';
+
+function App() {
+  return (
+    <div className='background'>
+      <MainContent />     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/topcontent.js
@@ -0,0 +1,11 @@
+import ArticleText from './ArticleText';
+
+function topcontent() {
+  return (
+      <section className='overlay-content'>
+          <ArticleText />
+      </section>     
+  );
+}
+
+export default topcontent;
